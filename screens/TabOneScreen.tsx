d816649d451cf2dae196ad1e3e4b450fb8653cee
jdnhix <jdhix98890@yahoo.com>
2022-01-26 15:12:01 -0600

    init

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -4,10 +4,10 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'Login'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home Tab</Text>
+      <Text style={styles.title}>Login</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
