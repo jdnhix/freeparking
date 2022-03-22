c72a2fe0beb4573a0ae560c8a9603138dc0b138e
yuxuanshi <yuxuan.shi@vanderbilt.edu>
2022-03-21 20:54:55 -0500

    edit spot page update

--- screens/EditSpotScreen.js
+++ screens/EditSpotScreen.js
@@ -1,7 +1,23 @@
 import * as React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Alert,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { AntDesign, Feather } from "@expo/vector-icons";
 import { COLORS } from "../components/Colors";
+import { useForm, Controller } from "react-hook-form";
+
+// TODO:
+// 2. How to add the spot to saved spot page
+// 3. (Remove functionality in saved spot page)
+// 4. (Edit functionality in saved spot page)
+// 5. Time availability design
 
 export default function EditSpotScreen({ navigation }) {
   // Go back to the saved spots page.
@@ -10,25 +26,163 @@ export default function EditSpotScreen({ navigation }) {
     navigation.navigate("Tabs");
   };
 
+  const toCamera = () => {
+    console.log("Bring out camera");
+  };
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      address: "",
+    },
+  });
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <View style={{ flex: 1, backgroundColor: "lightblue" }}>
-      <TouchableOpacity
-        style={styles.backBtn}
-        activeOpacity={0.8}
-        onPressOut={toSaved}
-      >
-        <View>
-          <AntDesign name="arrowleft" size={50} color={COLORS.red_theme} />
+    // Make keyboard disappear when clicked in blank spot
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={{ flex: 1 }}>
+        {/* The top two icons */}
+        <View style={styles.topBar}>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              alignItems: "flex-start",
+              justifyContent: "center",
+              marginLeft: "6%",
+            }}
+            activeOpacity={0.8}
+            onPressOut={toSaved}
+          >
+            <View>
+              <AntDesign name="arrowleft" size={50} color={COLORS.red_theme} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              alignItems: "flex-end",
+              justifyContent: "center",
+              marginRight: "7%",
+            }}
+            activeOpacity={0.8}
+            onPressOut={toCamera}
+          >
+            <View>
+              <Feather name="camera" size={40} color={COLORS.green_theme} />
+            </View>
+          </TouchableOpacity>
         </View>
-      </TouchableOpacity>
-    </View>
+
+        {/* The form with two input */}
+        <View style={styles.form}>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+              pattern: /^(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                placeholder="Spot Name"
+              />
+            )}
+            name="name"
+          />
+
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+              pattern: /^(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                placeholder="Spot Address"
+              />
+            )}
+            name="address"
+          />
+          {((errors.name && errors.name.type === "required") ||
+            (errors.address && errors.address.type === "required")) && (
+            <Text style={styles.errorMsg}>Both fields are required.</Text>
+          )}
+          {((errors.name && errors.name.type === "pattern") ||
+            (errors.address && errors.address.type === "pattern")) && (
+            <Text style={styles.errorMsg}>
+              Format error: alphanumeric and comma only.
+            </Text>
+          )}
+        </View>
+        {/* Check out DateTimePicker Expo */}
+
+        <TouchableOpacity
+          style={styles.saveBtn}
+          onPressOut={handleSubmit(onSubmit)}
+        >
+          <Text style={styles.saveBtnTxt}>Save</Text>
+        </TouchableOpacity>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
-  backBtn: {
+  topBar: {
+    height: 50,
+    marginTop: "13%",
+    flexDirection: "row",
+  },
+  // Save spot button
+  saveBtn: {
+    backgroundColor: COLORS.green_theme,
     position: "absolute",
-    top: "7%",
-    left: "7%",
+    width: 170,
+    height: 50,
+    bottom: "14%",
+    left: "30%",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 3,
+  },
+  // Add spot button text
+  saveBtnTxt: {
+    fontSize: 20,
+    color: "white",
+  },
+
+  input: {
+    fontSize: 20,
+    color: COLORS.green_theme,
+    borderBottomColor: COLORS.green_theme,
+    borderBottomWidth: 2,
+    // borderWidth: 1,
+    // borderColor: COLORS.green_theme,
+    width: "55%",
+    height: 40,
+    marginBottom: 20,
+  },
+
+  form: {
+    alignItems: "center",
+    marginTop: "25%",
+  },
+
+  errorMsg: {
+    color: COLORS.red_theme,
+    fontWeight: "300",
   },
 });
