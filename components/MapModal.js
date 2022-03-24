13882b5c2c3fc240590c0194214169edc23122a6
jdnhix <jdhix98890@yahoo.com>
2022-03-24 14:57:17 -0500

    start modal work

--- components/MapModal.js
+++ components/MapModal.js
@@ -1,27 +1,54 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Modal } from 'react-native';
 import Position from 'react-native/Libraries/Components/Touchable/Position';
+import { COLORS } from "../components/Colors";
+import { createOpenLink } from "react-native-open-maps";
 
 export default function MapModal(props) {
+
+    const toRoute = createOpenLink({ query: props.address });
+
     return (
-        <SafeAreaView>
-            <View style={styles.modal}>
-                <Text>Helloooo</Text>
-            </View>
-        </SafeAreaView>
+        <View style={styles.modal}>
+            <Text style={styles.spotNameText}>Spot Name</Text>
+            <Text style={styles.freeParkingText}>Free Parking</Text>
+            <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.button}
+                onPressOut={toRoute}
+            >
+                <Text style={styles.btnText}>Route</Text>
+            </TouchableOpacity>
+
+            
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     modal: {
-        width: '70%',
-        height: '60%',
+        width: 300,
+        height: 320,
+        backgroundColor: 'white',
         position: 'absolute',
-        top: 50,
-        left: 50,
-        zIndex: 10
-        
+        top: "15%",
+        borderRadius: 20,
+        flex: 1,
+        padding: 25,
+    },
+    spotNameText: {
+        fontWeight: 'bold',
+        fontSize: 30,
+        top: '5%',
+        color: COLORS.green_theme
+    },
+    freeParkingText: {
+        textDecorationLine: 'underline',
+        color: COLORS.green_theme,
+        fontSize: 22,
+        top: '15%'
     }
+
 
 
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, TouchableHighlightBase } from 'react-native';
 import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
 import MapModal from '../components/MapModal';
+import Modal from 'react-native-modal';
 
 const { height, width } = Dimensions.get( 'window' );
 const LATITUDE = 36.174465; // Nashville, TN
@@ -31,38 +32,122 @@ const markers = [
         latitude: 36.09,
         longitude: -86.74,
         name: "test"
-    }
+    },
+    {
+        latitude: 36.146450,
+        longitude: -86.803482,
+        name: "Vanderbilt",
+        address: "2301 Vanderbilt Place"
+    },
 ]
 
 
-export default function HomeScreen() {
-    return (
-        <View style={styles.center}>
-            <MapView style={styles.map}
-                region = {{
-                    latitude: LATITUDE,
-                    longitude: LONGITUDE,
-                    latitudeDelta: LATITUDE_DELTA,
-                    longitudeDelta: LONGITUDE_DELTA,
-                    showCompass: false,
-                    maxZoomLevel: 20
-                }}
-            >
-                {markers.map((spot, index) => (
-                    <Marker
-                        key={index}
-                        coordinate={{latitude: spot.latitude, longitude: spot.longitude}}
-                        onPress={ () => {console.log(`marker ${spot.name} pressed`)}}
-                    />
-                ))}
-            </MapView> 
-            <View style={{zIndex: 10, position: 'absolute'}}>
-                <MapModal/>
-            </View>
-            
-         </View>
-    );
+class HomeScreen extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.mapRef = null
+        this.state = {
+            modalVisible: false
+        }
+        this.modalOn = this.setModalVisible.bind(this)
+        this.modalOff = this.setModalNotVisible.bind(this)
+    }
+
+    setModalVisible(e) {
+        this.setState({modalVisible: true})
+    }
+
+    setModalNotVisible(e) {
+        this.setState({modalVisible: false})
+    }
+
+    render() {
+        return (
+            <View style={styles.center}>
+                <MapView style={styles.map}
+                    ref = {(ref) => {this.mapRef = ref}}
+                    initialRegion = {{
+                        latitude: LATITUDE,
+                        longitude: LONGITUDE,
+                        latitudeDelta: LATITUDE_DELTA,
+                        longitudeDelta: LONGITUDE_DELTA,
+                        showCompass: false,
+                        maxZoomLevel: 20
+                    }}
+                >
+                    {markers.map((spot, index) => (
+                        <Marker
+                            key={index}
+                            coordinate={{latitude: spot.latitude, longitude: spot.longitude}}
+                            onPress={ () => {
+                                console.log(`marker ${spot.name} pressed`)
+                                this.modalOn()
+                                // this.mapRef.fitToSuppliedMarkers(spot) todo get this working 
+                            
+                            }}
+                        />
+                    ))}
+                </MapView> 
+                <Modal
+                    style={styles.modal}
+                    visible={this.state.modalVisible}
+                    onBackdropPress= {() => this.modalOff()}
+                    animationType='fade'
+                >
+                    <MapModal/>
+                </Modal>
+             </View>
+        );
+    }
+
+
+
 }
+
+// export default function HomeScreen() {
+
+//     const [modalVisible, setModalVisible] = React.useState(false)
+//     const mapRef = React.useRef(null)
+
+
+//     return (
+//         <View style={styles.center}>
+//             <MapView style={styles.map}
+//                 ref = {mapRef}
+//                 initialRegion = {{
+//                     latitude: LATITUDE,
+//                     longitude: LONGITUDE,
+//                     latitudeDelta: LATITUDE_DELTA,
+//                     longitudeDelta: LONGITUDE_DELTA,
+//                     showCompass: false,
+//                     maxZoomLevel: 20
+//                 }}
+//             >
+//                 {markers.map((spot, index) => (
+//                     <Marker
+//                         key={index}
+//                         coordinate={{latitude: spot.latitude, longitude: spot.longitude}}
+//                         onPress={ () => {
+//                             console.log(`marker ${spot.name} pressed`)
+//                             setModalVisible(true)
+//                             mapRef.fitToSuppliedMarkers(spot)
+                        
+//                         }}
+//                     />
+//                 ))}
+//             </MapView> 
+//             <Modal
+//                 style={styles.modal}
+//                 visible={modalVisible}
+//                 onBackdropPress= {() => setModalVisible(false)}
+//                 animationType='fade'
+//             >
+//                 <MapModal/>
+//             </Modal>
+//          </View>
+//     );
+// }
 
 const styles = StyleSheet.create({
     center: {
@@ -73,6 +158,15 @@ const styles = StyleSheet.create({
         width: width,
         height: height,
         zIndex: -1
+    },
+    modal: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        // backgroundColor: 'blue',
     }
 
+
 });
+
+
+export default HomeScreen;
