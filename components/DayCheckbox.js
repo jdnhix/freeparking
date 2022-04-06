f1afa19781b85025788b265f8fc6ede6d0455fe0
jdnhix <jdhix98890@yahoo.com>
2022-04-06 17:21:58 -0500

    finish custom checkboxes

---
+++ components/DayCheckbox.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { COLORS } from "./Colors";
+
+export default function DayCheckbox({ checked, onChange, letter }) {
+  function onPress() {
+    onChange(!checked);
+  }
+
+  return (
+    <Pressable
+      style={[styles.checkboxBase, checked && styles.checkboxChecked]}
+      onPress={onPress}
+    >
+      <Text>{letter}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  checkboxBase: {
+    width: 30,
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    borderWidth: 2,
+    borderColor: COLORS.yellow_theme,
+    backgroundColor: "transparent",
+    marginRight: 9,
+  },
+
+  checkboxChecked: {
+    backgroundColor: COLORS.yellow_theme,
+  },
+});
--- components/TimeModal.js
+++ components/TimeModal.js
@@ -8,11 +8,50 @@ import {
   useEffect,
 } from "react-native";
 import { COLORS } from "../components/Colors";
+import DayCheckbox from "./DayCheckbox";
 
-export default function TimeModal(props) {
+const DAYS = ["M", "T", "W", "R", "F", "S", "U"];
+
+export default function TimeModal() {
+  const [checkedState, setCheckedState] = React.useState(
+    new Array(DAYS.length).fill(false)
+  );
+
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((day, index) =>
+      index === position ? !day : day
+    );
+
+    console.log(updatedCheckedState);
+    setCheckedState(updatedCheckedState);
+  };
+
   return (
     <View style={styles.modal}>
       <Text style={styles.text}>Time Slot</Text>
+      <View style={styles.checkboxRow}>
+        {DAYS.map((letter, index) => {
+          return (
+            <DayCheckbox
+              letter={letter}
+              checked={checkedState[index]}
+              onChange={() => handleOnChange(index)}
+            />
+          );
+        })}
+
+        {/* <DayCheckbox letter="M" />
+        <DayCheckbox letter="T" />
+        <DayCheckbox letter="W" />
+        <DayCheckbox letter="R" />
+        <DayCheckbox letter="F" />
+        <DayCheckbox letter="S" />
+        <DayCheckbox letter="U" /> */}
+      </View>
+      <View style={styles.timeSection}>
+        <Text style={styles.text}>Start: 2:00 PM</Text>
+        <Text style={styles.text}>End: 3:00 PM</Text>
+      </View>
     </View>
   );
 }
@@ -27,12 +66,20 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     flex: 1,
     padding: 25,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingHorizontal: 40,
   },
 
   text: {
     color: COLORS.green_theme,
-    fontSize: 20,
+    fontSize: 22,
+  },
+
+  checkboxRow: {
+    flexDirection: "row",
+    marginTop: 15,
+  },
+
+  timeSection: {
+    marginTop: 20,
   },
 });
